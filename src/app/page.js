2ba67eb1d9bd1449d2249f10891664eed801714c
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-02-22 09:02:36 +0100

    added the logic fot adding and editing

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,33 @@
 'use client';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import {list} from "postcss";
 const App = () => {
   const [userInput, setUserInput] = useState('');
   const [list, setList] = useState([]);
-  const [editIndex, setEditIndex] = useState(nulll);
+  const [editIndex, setEditIndex] = useState(null);
+
+  const updateInput = (value) => {
+    setUserInput(value);
+  }
+}
+
+const handleAction = () => {
+  if (userInput.trim() === '')return;
+
+  if (editIndex !== null) {
+    const updatedList = list.map((item, index) =>
+      index === editIndex ? { ...item, value: userInput} : item
+    );
+    setList(updatedList);
+    setEditIndex(null);
+  } else {
+    const newItem = {
+      id: Math.random(),
+      value: userInput
+    }
+    setList([...list, newItem]);
+  }
+  setUserInput('')
 }
 
 export default function Home() {
